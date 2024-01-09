9244a4901abb04e2a19236f606ceb0d510c27ebb
Nol75 <Nol75>
2024-01-09 20:38:44 +0000

    notebooks do pipeline

---
+++ notebooks/bronze_to_silver.scala
@@ -0,0 +1,69 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ##Conferindo se os dados foram montados e se tempos acesso a pasta bronze
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC dbutils.fs.ls("/mnt/dados/bronze")
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ##Lendo os dados na camada bronze
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/bronze/dataset_moveis/"
+val df = spark.read.format("delta").load(path)
+
+// COMMAND ----------
+
+display(df)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC # Transformando os campos do Json em colunas
+
+// COMMAND ----------
+
+display(df.select("anuncio.*"))
+
+// COMMAND ----------
+
+display(
+  df.select("anuncio.*", "anuncio.endereco.*")
+  )
+
+// COMMAND ----------
+
+val dados_detalhados = df.select("anuncio.*", "anuncio.endereco.*")
+
+// COMMAND ----------
+
+display(dados_detalhados)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC #Removendo Colunas
+
+// COMMAND ----------
+
+val df_silver = dados_detalhados.drop("caracteristicas", "endereco")
+display(df_silver)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC #Salvando na camada silver
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/silver/dataset_moveis"
+df_silver.write.format("delta").mode("overwrite").save(path)
+
+// COMMAND ----------
+
+
